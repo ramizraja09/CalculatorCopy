a8445a1b3d0843f3770e9744eeadf99da1aba5f2
Ramiz Raja <Ramiz.raja9@gmail.com>
2025-08-10 19:35:29 +0000

    Fix- Sleep Calculator Fix- Blood Sugar Converter Fix- Diabetes Risk Calc

--- src/components/calculators/diabetes-risk-calculator.tsx
+++ src/components/calculators/diabetes-risk-calculator.tsx
@@ -1,7 +1,6 @@
 
 "use client";
 
-import { useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { ShieldAlert } from 'lucide-react';
